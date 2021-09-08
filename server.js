7aec3b895c71ed92330f5eff7e1017804bfa776c
Ann <annkulakova96@gmail.com>
2021-09-08 07:46:23 +0300

    implemented interaction with the database via API

--- server.js
+++ server.js
@@ -1,31 +1,37 @@
-import { Posts } from "./data/data.js";
-import { MongoClient } from "mongodb";
-import * as dotenv from "dotenv";
-import express from "express";
+import { MongoClient } from 'mongodb'
+import * as dotenv from 'dotenv'
+import express from 'express'
 
-dotenv.config();
+dotenv.config()
 
-const port = process.env.PORT;
-const posts = new Posts();
+const uri = process.env.DB_CONNECTION_STRING
+console.log(uri)
+const client = new MongoClient(uri)
+const port = process.env.PORT
 
-const app = express();
-app.get("/get_posts", (req, res) => {
-  res.send(posts.getPosts());
-});
+const app = express()
+app.get('/get_posts', async (req, res) => {
+  const collection = client.db().collection('Posts')
+  const allPosts = await collection.find().toArray()
+  res.json(allPosts)
+})
 
-app.get("/send_post", (req, res) => {
+app.get('/send_post', async (req, res) => {
   const post = {
-    key: Math.random(),
     name: req.query.name,
     text: req.query.text,
-    date: Date.now(),
-  };
-  console.log(post);
-  res.json();
-});
+    timestamp: Date.now(),
+  }
+  const collection = client.db().collection('Posts')
+  await collection.insertOne(post)
+  res.send()
+})
 
 app.use((req, res) => {
-  res.status(404).send("Oops something going wrong :{");
-});
+  res.status(404).send('Oops something going wrong :{')
+})
 
-app.listen(port, "127.0.0.1");
+client.connect(() => {
+  app.listen(port, '127.0.0.1')
+  console.log('Server is running')
+})
