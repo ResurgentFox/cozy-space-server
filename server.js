60090c64e22fc0cc4e76644aa90a0ec9797e00da
Ann <annkulakova96@gmail.com>
2021-09-21 20:06:07 +0300

    posts listening implemented via websockets

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import { MongoClient } from 'mongodb'
 import * as dotenv from 'dotenv'
 import express from 'express'
 import cors from 'cors'
+import WebSocket from 'ws'
 
 dotenv.config()
 
@@ -10,7 +11,10 @@ const client = new MongoClient(uri)
 const port = process.env.PORT
 
 const app = express()
+const wsServer = new WebSocket.Server({ port: 3005 })
+
 app.use(cors({ origin: process.env.FRONTEND_DOMAIN }))
+
 app.get('/get_posts', async (req, res) => {
   const collection = client.db().collection('Posts')
   const allPosts = await collection.find().toArray()
@@ -24,7 +28,14 @@ app.get('/send_post', async (req, res) => {
     timestamp: Date.now(),
   }
   const collection = client.db().collection('Posts')
-  await collection.insertOne(post)
+  const { insertedId } = await collection.insertOne(post)
+  const newPost = {
+    _id: insertedId,
+    name: post.name,
+    text: post.text,
+    timestamp: post.timestamp
+  }
+  wsServer.clients.forEach(ws => ws.send(JSON.stringify(newPost)))
   res.send()
 })
 
@@ -35,4 +46,9 @@ app.use((req, res) => {
 client.connect(() => {
   app.listen(port, '127.0.0.1')
   console.log('Server is running')
+})
+
+app.on('close', () => {
+  client.close()
+  wsServer.close()
 })
